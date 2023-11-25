77d1530d88fcc490e3dc901faa555e3dabe25b5d
Terry Duong <tduong10101@gmail.com>
2023-11-25 21:57:40 +1100

    lesson-6-8 updated css, added onclick button

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log('hello, ninja!', e)
+  }
+  const handleClickAgain = (name,e) => {
+    console.log('hello '+name,e.target)
+  }
   return ( 
     <div className="home">
       <h2>Home Page</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e)=>handleClickAgain('Mario', e)}>Click me again</button>
     </div>
   );
 }
