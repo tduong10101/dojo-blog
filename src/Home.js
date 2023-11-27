42964815087ec7a86d86c34e953febd2cdefc3ae
Terry Duong <tduong10101@gmail.com>
2023-11-27 22:48:16 +1100

    lesson-20 add error handling

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,30 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
   const [isPending, setIsPending] = useState(true);
+  const [error,setError]=useState(null)
 
   useEffect(() => {
     fetch('http://localhost:5000/blogs')
       .then(res => {
+        console.log(res);
+        if(!res.ok) {
+          throw Error(`could not fetch the data for the resource - ${res.status} - ${res.statusText}`)
+        }
         return res.json();
       })
       .then((data => {
         setBlogs(data);
         setIsPending(false);
+        setError(null)
       }))
+      .catch(err => {
+        setError(err.message)
+      })
   }, []);
   return (
     <div className="home">
-      { isPending && <div>Loading...</div>}
+      { error && <div>{error}</div>}
+      { !error && isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
       {/* <button onClick={() => setName('luigi')}>change name</button> */}
     </div>
