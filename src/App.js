de3ca22b44963b186f453e7e7534ab81ff4e8864
Terry Duong <tduong10101@gmail.com>
2023-11-29 22:01:56 +1100

    lesson-22 adding route and use exact attribute

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   const title = 'Welcome to the new blog';
@@ -11,8 +12,11 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home/>
+            </Route>
+            <Route path="/create">
+              <Create />
             </Route>
           </Switch>
         </div>
