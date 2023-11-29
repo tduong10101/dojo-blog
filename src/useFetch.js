c4c6b13fbba910565ba49bb70d8316113958d3a0
Terry Duong <tduong10101@gmail.com>
2023-11-29 22:31:26 +1100

    lesson-24 abortcontroller

--- src/useFetch.js
+++ src/useFetch.js
@@ -7,23 +7,31 @@ const useFetch = (url) => {
   const [error,setError]=useState(null)
   
   useEffect(() => {
-    fetch(url)
-      .then(res => {
-        console.log(res);
-        if(!res.ok) {
-          throw Error(`could not fetch the data for the resource - ${res.status} - ${res.statusText}`)
-        }
-        return res.json();
-      })
-      .then((data => {
-        setData(data);
-        setIsPending(false);
-        setError(null)
-      }))
-      .catch(err => {
-        setIsPending(false);
-        setError(err.message)
-      })
+    const abortCont = new AbortController();
+    setTimeout(() => {
+      fetch(url, { signal: abortCont.signal})
+        .then(res => {
+          console.log(res);
+          if(!res.ok) {
+            throw Error(`could not fetch the data for the resource - ${res.status} - ${res.statusText}`)
+          }
+          return res.json();
+        })
+        .then((data => {
+          setData(data);
+          setIsPending(false);
+          setError(null)
+        }))
+        .catch(err => {
+          if (err.name === 'AbortError'){
+            console.log('fetch aborted');
+          } else {
+            setIsPending(false);
+            setError(err.message)
+          }
+        })
+    }, 1000);
+    return () => abortCont.abort();
   }, [url]);
   return {data, isPending, error}
 }
