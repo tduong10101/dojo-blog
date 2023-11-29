4a959bf9fed57a51c302f151126d797406ee2d80
Terry Duong <tduong10101@gmail.com>
2023-11-29 22:47:37 +1100

    lesson-25 useParams

---
+++ src/BlogDetails.js
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom"
+const BlogDetails = () => {
+
+  const {id}= useParams()
+  return ( 
+    <div className="blog-detail">
+      <h2>Blog-detail {id}</h2>
+    </div>
+  );
+}
+
+export default BlogDetails;
