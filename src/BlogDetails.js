543503ec6981f36773df59996405511fdfbfbc5a
Terry Duong <tduong10101@gmail.com>
2024-01-10 21:13:49 +1100

    added blogdetails

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,20 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import useFetch from './useFetch';
 const BlogDetails = () => {
 
-  const {id}= useParams()
+  const {id}= useParams();
+  const { data: blog, error, isPending } = useFetch('http://localhost:5000/blogs/'+id);
   return ( 
-    <div className="blog-detail">
-      <h2>Blog-detail {id}</h2>
+    <div className="blog-details">
+      { isPending && <div>Loading ...</div> }
+      { error && <div>Error: { error }</div> }
+      { blog && (
+        <article>
+          <h2>{ blog.title }</h2>
+          <p>Writen by: { blog.author }</p>
+          <div> { blog.body }</div>
+        </article>
+      ) }
     </div>
   );
 }
