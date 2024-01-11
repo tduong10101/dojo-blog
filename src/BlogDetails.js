c840512c11832286b9ab0f3d44c4e19823ff6cb8
Terry Duong <tduong10101@gmail.com>
2024-01-11 22:27:38 +1100

    added delete blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,18 @@
 import { useParams } from "react-router-dom";
 import useFetch from './useFetch';
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 const BlogDetails = () => {
 
   const {id}= useParams();
   const { data: blog, error, isPending } = useFetch('http://localhost:5000/blogs/'+id);
+  const history = useHistory();
+  const handleClick = () => {
+    fetch('http://localhost:5000/blogs/'+blog.id, {
+      method: 'DELETE'
+    }).then(()=>{
+      history.push('/');
+    })
+  }
   return ( 
     <div className="blog-details">
       { isPending && <div>Loading ...</div> }
@@ -13,6 +22,7 @@ const BlogDetails = () => {
           <h2>{ blog.title }</h2>
           <p>Writen by: { blog.author }</p>
           <div> { blog.body }</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       ) }
     </div>
